3961393c8c93a53905d4078c2cfcca306d25434b
Chris Mahoney <chrism@unity3d.com>
2016-01-12 11:52:59 -0600

    First commit

---
+++ Assets/Editor/PostProcessBuilder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Collections;
+using System.IO;
+
+public class PostProcessBuilder {
+
+	public static void OnPostProcessBuild()
+	{
+		throw new IOException("This is an exception");
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# PostProcessFailure
